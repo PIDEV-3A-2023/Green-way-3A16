46b3ef1c83cd25fc6e342aca8ba0d707ab918bfe
KhalilElbehi <khalil.elbehi@esprit.tn>
2023-04-10 23:51:50 +0100

    crud

--- src/Entity/Event.php
+++ src/Entity/Event.php
@@ -2,47 +2,95 @@
 
 namespace App\Entity;
 
-use Symfony\Component\Validator\Constraints as Assert;
-use Doctrine\DBAL\Types\Types;
 use App\Repository\EventRepository;
-use App\Repository\Repository;
-use App\Entity\Event;
+use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
-use Doctrine\Common\Collections\ArrayCollection;
+
 #[ORM\Entity(repositoryClass: EventRepository::class)]
 class Event
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
-    #[Assert\NotBlank(message:"peut pas etre vide")]
-    private ?int $id=null;
-    
+    private ?int $id = null;
 
-    #[ORM\Column(length: 500)]
-    #[Assert\NotBlank(message:"peut pas etre vide")]
-    private ?string $nom= null;
-    
+    #[ORM\Column(length: 255)]
+    private ?string $nom = null;
 
-    #[ORM\Column(type: Types::DATE_MUTABLE)]
-    #[Assert\NotBlank(message:"date peut pas etre vide")]
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
     private ?\DateTimeInterface $datedeb = null;
-    
 
-    #[ORM\Column(type: Types::DATE_MUTABLE)]
-    #[Assert\NotBlank(message:"date peut pas etre vide")]
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
     private ?\DateTimeInterface $datefin = null;
-    
-    
 
-    #[ORM\Column(length: 500)]
-    #[Assert\NotBlank(message:"peut pas etre vide")]
-    private ?string $description= null;
-   
+    #[ORM\Column(length: 255)]
+    private ?string $description = null;
 
-    #[ORM\Column(length: 500)]
-    #[Assert\NotBlank(message:"peut pas etre vide")]
-    private ?string $image= null;
+    #[ORM\Column(length: 255)]
+    private ?string $image = null;
 
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
 
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getDatedeb(): ?\DateTimeInterface
+    {
+        return $this->datedeb;
+    }
+
+    public function setDatedeb(\DateTimeInterface $datedeb): self
+    {
+        $this->datedeb = $datedeb;
+
+        return $this;
+    }
+
+    public function getDatefin(): ?\DateTimeInterface
+    {
+        return $this->datefin;
+    }
+
+    public function setDatefin(\DateTimeInterface $datefin): self
+    {
+        $this->datefin = $datefin;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(string $image): self
+    {
+        $this->image = $image;
+
+        return $this;
+    }
 }
