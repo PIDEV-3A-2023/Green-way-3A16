e58f1a0cf0c2347f8fed1c6b0f43ee8c40cdc069
KhalilElbehi <khalil.elbehi@esprit.tn>
2023-04-26 10:55:20 +0100

    commit3
    ajouter le tri

--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -9,6 +9,11 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Dompdf\Dompdf;
+use Dompdf\Options;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Box\Spout\Writer\Common\Creator\WriterEntityFactory;
+use Knp\Component\Pager\PaginatorInterface;
 
 #[Route('/event')]
 class EventController extends AbstractController
@@ -20,12 +25,153 @@ class EventController extends AbstractController
             'events' => $eventRepository->findAll(),
         ]);
     }
+    #[Route('/sort-by-datedeb', name: 'app_event_sort_by_datedeb', methods: ['GET'])]
+
+   
+   public function sortByDatedeb(Request $request,PaginatorInterface $paginator,EventRepository $eventRepository): Response
+   {    $query = $this->getDoctrine()->getRepository(Event::class)->createQueryBuilder('e');
+    $query->orderBy('e.datedeb', 'ASC');
+
+    $events = $paginator->paginate(
+        $query->getQuery(),
+        $request->query->getInt('page', 1),
+        10 // number of items per page
+    );
+       // Query the database for events sorted by "datedeb" in ascending order
+
+       // Render the sorted events in the Twig template
+       return $this->render('event/admin.html.twig', [
+           'events' => $events,
+       ]);
+   }
+   #[Route('/sort-by-nom', name: 'app_event_sort_by_nom', methods: ['GET'])]
+
+   
+   public function sortByNom(Request $request,PaginatorInterface $paginator,EventRepository $eventRepository): Response
+   {    $query = $this->getDoctrine()->getRepository(Event::class)->createQueryBuilder('e');
+    $query->orderBy('e.nom', 'ASC');
+
+    $events = $paginator->paginate(
+        $query->getQuery(),
+        $request->query->getInt('page', 1),
+        10 // number of items per page
+    );
+       // Query the database for events sorted by "datedeb" in ascending order
+
+       // Render the sorted events in the Twig template
+       return $this->render('event/admin.html.twig', [
+           'events' => $events,
+       ]);
+   }
+
+
     #[Route('/admin', name: 'app_event_admin', methods: ['GET'])]
-    public function index2(EventRepository $eventRepository): Response
+    public function index2(Request $request,PaginatorInterface $paginator,EventRepository $eventRepository): Response
     {
-        return $this->render('event/admin.html.twig', [
-            'events' => $eventRepository->findAll(),
+
+        // Get the search term from the query string
+    $searchTerm = $request->query->get('search');
+
+    // Query the database for events that match the search term
+    $query = $this->getDoctrine()->getRepository(Event::class)->createQueryBuilder('e');
+    if ($searchTerm) {
+        $query->where('e.nom LIKE :searchTerm')
+        ->orWhere('e.datedeb LIKE :searchTerm')
+        ->orWhere('e.datefin LIKE :searchTerm')
+        ->setParameter('searchTerm', '%' . $searchTerm . '%');
+            
+    }
+    $query->orderBy('e.id', 'DESC');
+
+    // Use the KnpPaginatorBundle to paginate the query results
+    $events = $paginator->paginate(
+        $query->getQuery(),
+        $request->query->getInt('page', 1),
+        10 // number of items per page
+    );
+
+    // Render the events in the Twig template
+    return $this->render('event/admin.html.twig', [
+        'events' => $events,
+    ]);
+    }
+        
+    
+
+    #[Route('/exel', name: 'app_event_exel', methods: ['GET'])]
+   
+    public function indexexel(EventRepository $eventRepository): Response
+    {
+        $events = $eventRepository->findAll();
+    
+        $path = 'C:\exel\events.xlsx';
+        $writer = \Box\Spout\Writer\Common\Creator\WriterEntityFactory::createXLSXWriter();
+        $writer->openToFile($path);
+        $header = ['id', 'nom', 'datedeb', 'datefin', 'description', 'image'];
+        $rowFromValues = WriterEntityFactory::createRowFromArray($header);
+        $writer->addRow($rowFromValues);
+        foreach ($events as $event) {
+            $data = [
+                'id' => $event->getId(),
+                'nom' => $event->getNom(),
+                'datedeb' => $event->getDatedeb() ? $event->getDatedeb()->format('Y-m-d H:i:s') : null,
+                'datefin' => $event->getDatefin() ? $event->getDatefin()->format('Y-m-d H:i:s') : null,
+                'description' => $event->getDescription(),
+                'image' => $event->getImage()
+            ];
+            
+          
+            
+            $rowFromValues = WriterEntityFactory::createRowFromArray($data);
+            $writer->addRow($rowFromValues);
+        }
+        
+        $writer->close();
+        $this->addFlash('success', 'La feuille Excel est prête. Vérifiez ' . $path);
+        return $this->redirectToRoute('app_event_admin', [], Response::HTTP_SEE_OTHER);
+    }
+    #[Route('/DownloadEvenementsData', name: 'DownloadEvenementsData', methods: ['GET'])]
+
+    public function DownloadEvenementsData(EventRepository $repository)
+    {
+        $evenements=$repository->findAll();
+        $options = new OptionsResolver();
+
+        // On définit les options du PDF
+        $pdfOptions = new Options();
+        // Police par défaut
+        $pdfOptions->set('defaultFont', 'Arial');
+        $pdfOptions->setIsRemoteEnabled(true);
+
+        // On instancie Dompdf
+        $dompdf = new Dompdf($pdfOptions);
+        $context = stream_context_create([
+            'ssl' => [
+                'verify_peer' => FALSE,
+                'verify_peer_name' => FALSE,
+                'allow_self_signed' => TRUE
+            ]
         ]);
+        $dompdf->setHttpContext($context);
+
+        // On génère le html
+        $html = $this->renderView('event/download.html.twig',
+            ['evenements'=>$evenements ]);
+
+
+        $dompdf->loadHtml($html);
+        $dompdf->setPaper('A4', 'portrait');
+        $dompdf->render();
+
+        // On génère un nom de fichier
+        $fichier = 'Tableau des Produits.pdf';
+
+        // On envoie le PDF au navigateur
+        $dompdf->stream($fichier, [
+            'Attachment' => true
+        ]);
+
+        return new Response();
     }
 
     #[Route('/new', name: 'app_event_new', methods: ['GET', 'POST'])]
@@ -82,4 +228,6 @@ class EventController extends AbstractController
 
         return $this->redirectToRoute('app_event_index', [], Response::HTTP_SEE_OTHER);
     }
+   
+
 }
--- src/Entity/Event.php
+++ src/Entity/Event.php
@@ -12,21 +12,27 @@ class Event
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
+    #[Assert\NotBlank(message:"peut pas etre vide")]
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
+    #[Assert\NotBlank(message:"peut pas etre vide")]
     private ?string $nom = null;
 
     #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    #[Assert\NotBlank(message:"date peut pas etre vide")]
     private ?\DateTimeInterface $datedeb = null;
 
     #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    #[Assert\NotBlank(message:"date peut pas etre vide")]
     private ?\DateTimeInterface $datefin = null;
 
     #[ORM\Column(length: 255)]
+    #[Assert\NotBlank(message:"peut pas etre vide")]
     private ?string $description = null;
 
     #[ORM\Column(length: 255)]
+    #[Assert\NotBlank(message:"peut pas etre vide")]
     private ?string $image = null;
 
     public function getId(): ?int
